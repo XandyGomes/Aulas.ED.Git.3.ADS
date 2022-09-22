fe863bf07cbb5b1a64913e083f295e11ecbdf0a4
XandyGomes <x3.computadores@hotmail.com>
2022-09-22 20:26:12 -0300

    Aula.22.09.2022

--- mergeSorte-obj.mjs
+++ mergeSorte-obj.mjs
@@ -1,19 +1,20 @@
-function mergeSort(vetor){
+function mergeSort(vetor, fnComp){
     if(vetor.length < 2) return vetor
     let meio = Math.floor(vetor.length / 2)
 
     let vetEsq = vetor.slice(0, meio)
     let vetDir = vetor.slice(meio)
 
-    vetEsq = mergeSort(vetEsq)
-    vetDir = mergeSort(vetDir)
+
+    vetEsq = mergeSort(vetEsq, fnComp)
+    vetDir = mergeSort(vetDir, fnComp)
 
     let posEsq = 0
     let posDir = 0
     let vetRes = []
 
     while(posEsq < vetEsq.length && posDir < vetDir.length){
-        if(vetEsq[posEsq] < vetDir[posDir]){
+        if(fnComp(, vetEsq[posEsq], vetDir[posDir])){
             vetRes.push(vetEsq[posEsq])
             posEsq++
         }
@@ -35,8 +36,10 @@ function mergeSort(vetor){
 
 }
 
-let nums = [ 5, 3 , 1 , 4 , 2 ]
+import { objMotoristas } from './data/motoristas-obj-desord.mjs'
 
-let numsOrd = mergeSort(nums)
+// let ord = mergeSort(objMotoristas, (elem1, elem2) => elem1.nome_motorista > elem2.nome_motorista)
 
-console.log({numsOrd})
+let ord = mergeSort(objMotoristas, (elem1, elem2) => elem1.nome_motorista.localeCompare(elem2.nome_motorista, 'pt-br') <= 0)
+
+console.log({ord})
